46d90a53b5636db14cf08ab3ed756abefe380bb5
lavi-lena <elenamartynova26@mail.ru>
2026-08-05 15:41:26 +0300

    clean main

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,11 @@
-import tasks.Wallet.InsufficientFundsException;
-import tasks.Wallet.Wallet;
+import tasks.FilterBoxes.Box;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        Wallet wallet = new Wallet(50);
-        try {
-            wallet.buyItem(-23);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
 
 
-        try {
-            wallet.buyItem(69);
-        } catch (InsufficientFundsException s){
-            System.out.println("Мы словили ошибку: "+s.getMessage());
-
-        }
     }
 }
