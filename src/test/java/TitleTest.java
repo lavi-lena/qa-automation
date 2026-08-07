bb5548b8cbbf4ceeaf23c98d40fc2b68b69c921c
lavi-lena <elenamartynova26@mail.ru>
2026-08-07 16:28:58 +0300

    rename FirstTest.java to TitleTest.java and add test code

---
+++ src/test/java/TitleTest.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class TitleTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void testGoogleTitle() {
+        driver.get("https://google.com");
+        String actualTitle = driver.getTitle();
+        Assertions.assertEquals("Google", actualTitle);
+    }
+
+    @AfterEach
+    public void teardown(){
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
+
+
+
