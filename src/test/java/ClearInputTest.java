fc491ef386f1e8f68981780e35f17501747eae77
lavi-lena <elenamartynova26@mail.ru>
2026-08-11 15:35:46 +0300

    add ClearInputTest

---
+++ src/test/java/ClearInputTest.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import java.time.Duration;
+
+public class ClearInputTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    @Test
+    public void testClearAndReType() {
+        driver.get("https://saucedemo.com");
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        usernameField.sendKeys("wrong_user");
+        usernameField.clear();
+        usernameField.sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
