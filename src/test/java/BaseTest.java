5864b50f611401c703045b5282f2e0bc711a8c8b
lavi-lena <elenamartynova26@mail.ru>
2026-08-14 15:28:57 +0300

    update login in loginToSaucedemo

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -24,7 +24,7 @@ public class BaseTest {
     }
     protected void loginToSaucedemo(){
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
     }
