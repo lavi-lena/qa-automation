e53f3e3a689bbf91d42c56f8fa1527a947a15b93
lavi-lena <elenamartynova26@mail.ru>
2026-08-18 16:43:30 +0300

    add ArrayPractice isMirrored method

--- src/main/java/tasks/ArrayPractice/ArrayPractice.java
+++ src/main/java/tasks/ArrayPractice/ArrayPractice.java
@@ -155,5 +155,13 @@ public class ArrayPractice {
         }
         return array;
     }
+    public static boolean isMirrored (int[] array){
+        for (int i=0; i<array.length/2;i++){
+            if(array[i] != array[array.length -1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
