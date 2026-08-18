c8b0fd6e94506285f8f96d88b779fab7b6a9e59a
lavi-lena <elenamartynova26@mail.ru>
2026-08-18 14:19:24 +0300

    add ArrayPractice isGreaterThanLeftNeighbor method

--- src/main/java/tasks/ArrayPractice/ArrayPractice.java
+++ src/main/java/tasks/ArrayPractice/ArrayPractice.java
@@ -137,5 +137,14 @@ public class ArrayPractice {
         return result;
 
     }
+    public static int isGreaterThanLeftNeighbor(int[]array){
+       int count = 0;
+        for (int i=0;i<array.length-1;i++){
+            if (array[i+1]>array[i]){
+                count = count +1;
+            }
+        }
+        return count;
+    }
 }
 
